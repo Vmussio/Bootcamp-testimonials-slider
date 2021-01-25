ad920e0b57d632e38d620db7359484378ce0d978
Vmussio <mussiovalen@gmail.com>
2021-01-24 21:27:24 -0300

    Finishing the project

--- Slider test/app.js
+++ Slider test/app.js
@@ -2,6 +2,7 @@ const track = document.querySelector('.carousel__track');
 const slides = Array.from(track.children);
 const nextButton = document.querySelector('.carousel__button--right');
 const prevButton = document.querySelector('.carousel__button--left');
+console.log(nextButton, prevButton)
 const slideWidth = slides[0].getBoundingClientRect().width;
 //arrange the slides next to one another
 function setSlidesPosition() {
@@ -15,28 +16,24 @@ setSlidesPosition(slides);
 
 function moveToSlide(track, currentSlide, targetSlide) {
     track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+    console.log(targetSlide.style.left)
     currentSlide.classList.remove('current-slide');
     targetSlide.classList.add('current-slide');
 }
+
 //when I click left, move slides to the left
 prevButton.addEventListener('click', e => {
     const currentSlide = track.querySelector('.current-slide')
-    if (currentSlide.previousElementSibling) {
-        prevSlide = currentSlide.previousElementSibling;
-    }
-    else {
-        prevSlide = currentSlide.nextElementSibling;
-    }
+    const prevSlide = currentSlide.previousElementSibling;
     moveToSlide(track, currentSlide, prevSlide)
+    prevButton.classList.add('is-hidden')
+    nextButton.classList.remove('is-hidden')
 })
 //when I click right, move slides to the right
 nextButton.addEventListener('click', e => {
     const currentSlide = track.querySelector('.current-slide')
-    if (currentSlide.nextElementSibling) {
-        nextSlide = currentSlide.nextElementSibling;
-    }
-    else {
-        nextSlide = currentSlide.previousElementSibling;
-    }
+    const nextSlide = currentSlide.nextElementSibling;
     moveToSlide(track, currentSlide, nextSlide);
+    prevButton.classList.remove('is-hidden')
+    nextButton.classList.add('is-hidden')
 })
